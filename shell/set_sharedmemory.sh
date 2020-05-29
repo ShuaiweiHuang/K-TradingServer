91b8f711f7d015e6286ec73adeb4d53ee6cb7904
Keanu Huang <keenu1227@gmail.com>
2020-05-29 14:29:22 +0000

    [CVQuote] modify binance epoch format
    :Release Notes:
    
    :Detailed Notes:
    - Problem :
    - RootCause :
    - Solution :
    - Author : Keanu Huang
    - Build :
    
    :Testing Preformed:
    How to Test :

--- shell/set_sharedmemory.sh
+++ shell/set_sharedmemory.sh
@@ -28,20 +28,26 @@ cd $SCRIPTPATH
 #----------- Take the date of last code and plus on NUM.  -----------#
 DateCode=$(date +"%-m%d")
 SHIFT_BIT=100000
-if [[ $HOSTNAME == "dev1-hq" ]]; then
+if [[ $HOSTNAME == "server-debug" ]]; then
+    NUM=$(( 1020000000000 + $[DateCode * SHIFT_BIT]))
+elif [[ $HOSTNAME == "tm0-hq" ]]; then
     NUM=$(( 1020000000000 + $[DateCode * SHIFT_BIT]))
 elif [[ $HOSTNAME == "tm1-hq" ]]; then
     NUM=$(( 1030000000000 + $[DateCode * SHIFT_BIT]))
 elif [[ $HOSTNAME == "tm2-hq" ]]; then
     NUM=$(( 1040000000000 + $[DateCode * SHIFT_BIT]))
+elif [[ $HOSTNAME == "tm0-ir" ]]; then
+    NUM=$(( 8000000000000 + $[DateCode * SHIFT_BIT]))
 elif [[ $HOSTNAME == "tm1-ir" ]]; then
     NUM=$(( 8010000000000 + $[DateCode * SHIFT_BIT]))
 elif [[ $HOSTNAME == "tm2-ir" ]]; then
     NUM=$(( 8020000000000 + $[DateCode * SHIFT_BIT]))
 elif [[ $HOSTNAME == "tm1-hk" ]]; then
     NUM=$(( 8030000000000 + $[DateCode * SHIFT_BIT]))
-elif [[ $HOSTNAME == "tm2-hk" ]]; then
+elif [[ $HOSTNAME == "tm1-hk" ]]; then
     NUM=$(( 8040000000000 + $[DateCode * SHIFT_BIT]))
+elif [[ $HOSTNAME == "tm2-hk" ]]; then
+    NUM=$(( 8050000000000 + $[DateCode * SHIFT_BIT]))
 else
     NUM=$(( 9990000000000 + $[DateCode * SHIFT_BIT]))
     echo -e "${COLOR_RED}Host Wrong!!${COLOR_REST}"
